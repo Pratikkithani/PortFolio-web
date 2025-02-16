327980df21c831658df2963a09cb55a7861d2a68
pratikkithani <100682866+Pratikkithani@users.noreply.github.com>
2025-02-16 23:52:42 +0530

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
 
+
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
     burger.classList.toggle('toggle');
@@ -13,3 +14,30 @@ navLinks.forEach(link => {
         burger.classList.remove('toggle');
     });
 });
+
+
+const navLinksa = document.querySelectorAll('.nav-links li a');
+const button = document.getElementById("darkmode");
+button.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    document.getElementById('about').classList.toggle("dark-mode");
+    document.getElementById('projects').classList.toggle("dark-mode");
+    document.getElementById('certifications').classList.toggle("dark-mode");
+    document.getElementById('logos').classList.toggle("dark-mode");
+    document.getElementById('footer').classList.toggle("dark-mode");
+    document.getElementById('header').classList.toggle("dark-mode");
+    navLinksa.forEach(link=>{
+        link.classList.toggle("dark-mode");
+    })
+    document.querySelector('#start h1').classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        button.src = "images/day-mode.png";
+    } else {
+        button.src = "images/night-mode.png";
+    }
+
+    burger.classList.toggle('burger-color');
+            
+});        
+
